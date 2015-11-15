7e08e5f5b895d80330e8a0af185308d6a9b360c1
OnceMore2020 <guanhwang2011@gmail.com>
2015-11-15 08:38:42 +0000

    update content

---
+++ books/https.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+    var host = "oncemore2020.github.io";
+    if ((host == window.location.host) && (window.location.protocol != "https:"))
+        window.location.protocol = "https";
+</script>
