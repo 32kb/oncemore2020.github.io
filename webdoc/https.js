8f10b7ef18f93240647b68df189b8d7cc1409bac
OnceMore2020 <guanhwang2011@gmail.com>
2015-11-15 09:29:15 +0000

    update content

--- webdoc/https.js
+++ webdoc/https.js
@@ -1,5 +1,3 @@
-<script type="text/javascript">
-    var host = "oncemore2020.github.io";
-    if ((host == window.location.host) && (window.location.protocol != "https:"))
-        window.location.protocol = "https";
-</script>
+var host = "oncemore2020.github.io";
+if ((host == window.location.host) && (window.location.protocol != "https:"))
+    window.location.protocol = "https";
